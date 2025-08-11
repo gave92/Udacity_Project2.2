a951281be74be43ce6a96149201d63f0d46b996b
Marco Gavelli <marcogavelli2@gmail.com>
2025-08-11 21:43:35 +0200

    update filter.py

--- student/filter.py
+++ student/filter.py
@@ -49,17 +49,15 @@ class Filter:
         # TODO Step 1: implement and return process noise covariance Q
         ############
 
-        _F = self.F()
-        _Q = np.diag([0., 0., 0., self.q, self.q, self.q])
-        _integral_factor = np.array([
-            [self.dt / 3, 0., 0., self.dt / 2, 0., 0.],
-            [0., self.dt / 3., 0., 0., self.dt / 2, 0.],
-            [0., 0., self.dt / 3, 0., 0., self.dt / 2],
-            [self.dt / 2, 0., 0., self.dt, 0., 0.],
-            [0., self.dt / 2, 0., 0., self.dt, 0.],
-            [0., 0., self.dt / 2, 0., 0., self.dt]])
-        QT = _integral_factor * np.matmul(_F @ _Q, _F.T)
-        return QT.T
+        q_1 = (self.dt ** 3) * self.q / 3.0
+        q_2 = (self.dt ** 2) * self.q / 2.0
+        q_3 = self.dt * self.q
+        return np.matrix([[q_1, 0.0, 0.0, q_2, 0.0, 0.0],
+                          [0.0, q_1, 0.0, 0.0, q_2, 0.0],
+                          [0.0, 0.0, q_1, 0.0, 0.0, q_2],
+                          [q_2, 0.0, 0.0, q_3, 0.0, 0.0],
+                          [0.0, q_2, 0.0, 0.0, q_3, 0.0],
+                          [0.0, 0.0, q_2, 0.0, 0.0, q_3]])
         
         ############
         # END student code
@@ -73,7 +71,7 @@ class Filter:
         _F = self.F()
         _Q = self.Q()
         _x = _F @ track.x
-        _P = np.matmul(_F @ track.P, _F.T) + _Q
+        _P = _F @ track.P @ _F.T + _Q
         track.set_x(_x)
         track.set_P(_P)
         
@@ -89,11 +87,11 @@ class Filter:
         _gamma = self.gamma(track, meas)
         _H = meas.sensor.get_H(track.x)
         _S = self.S(track, meas, _H)
-        _K = np.matmul(track.P @ _H.T, np.linalg.inv(_S))
+        _K = track.P @ _H.T @ np.linalg.inv(_S)
         _x = track.x + _K @ _gamma
         track.set_x(_x)
-        _I = np.identity(n=self.dim_state)
-        _P = (_I - np.matmul(_K, _H)) @ track.P
+        _I = np.identity(self.dim_state)
+        _P = (_I - (_K @ _H)) @ track.P
         track.set_P(_P)
         
         ############
@@ -117,7 +115,7 @@ class Filter:
         # TODO Step 1: calculate and return covariance of residual S
         ############
 
-        return np.matmul(H @ track.P, H.T) + meas.R
+        return H @ track.P @ H.T + meas.R
         
         ############
         # END student code
